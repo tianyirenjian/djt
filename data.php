02f1fe41baedb1831f353cde604ea10025e6a832
tianyirenjian <179063828@qq.com>
2019-12-02 15:38:23 +0800

    use pdo

--- data.php
+++ data.php
@@ -1,5 +1,4 @@
 <?php
-	$conn=mysql_connect("localhost","symhytwg_fun","wahaha2019"); //连接数据库地址、用户名、密码
-	mysql_query("set names 'utf8'"); //数据库编码
-	mysql_select_db("sy_nows"); //数据库名称
+$dsn = "mysql:dbname=djt;host=mysql;charset=utf8";
+$pdo = new PDO($dsn, 'root', 'tianyirenjian');
 ?>
